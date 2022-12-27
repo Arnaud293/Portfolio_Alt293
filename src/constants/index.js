2c69e1db9573178c7dbe6da26829685d268b9fca
Arnaud293 <arnaudlt293@gmail.com>
2022-12-27 15:27:43 +0100

    create projects comp, add const with projectsData, import assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -57,4 +57,70 @@ export const links = [
         id: 5,
         link : 'contact'
     }
+];
+
+export const ProjectsData = [
+    {
+        id: 1,
+        imgUrl:'../assets/portfolio/metaversus.png',
+        title: 'Metaversus',
+        demoUrl: 'https://precious-malabi-33d670.netlify.app',
+        codeUrl: 'https://github.com/Arnaud293/NextJs_Metaverse_LandingPage'
+    },
+    {
+        id: 2,
+        imgUrl:'../assets/portfolio/clone-tube.png',
+        title: 'CloneTube',
+        demoUrl: 'https://sprightly-tiramisu-e0edbf.netlify.app',
+        codeUrl: 'https://github.com/Arnaud293/ReactApp_Youtube-clone'
+    },
+    {
+        id: 3,
+        imgUrl:'../assets/portfolio/nitflex.png',
+        title: 'Nitflex',
+        demoUrl: 'https://sparkling-youtiao-7bb6c8.netlify.app',
+        codeUrl: 'https://github.com/Arnaud293/ReactApp_movieAPI_NetflixClone'
+    },
+    {
+        id: 4,
+        imgUrl:'../assets/portfolio/crypto_desktop.png',
+        title: 'Watch Tower',
+        demoUrl: 'https://warm-biscotti-e182f7.netlify.app',
+        codeUrl: 'https://github.com/Arnaud293/ReactJs_CryptoApp'
+    },
+    {
+        id: 5,
+        imgUrl:'../assets/portfolio/theresto.png',
+        title: 'The Resto',
+        demoUrl: 'https://arnaud293.github.io/Scss_Model_Integration_The-Resto/',
+        codeUrl: 'https://github.com/Arnaud293/Scss_Model_Integration_The-Resto'
+    },
+    {
+        id: 6,
+        imgUrl:'../assets/portfolio/groupomania.png',
+        title: 'Groupomania',
+        demoUrl: '',
+        codeUrl: 'https://github.com/Arnaud293/P7_OC_DevWeb'
+    },
+    {
+        id: 7,
+        imgUrl:'../assets/portfolio/kanap.png',
+        title: 'Kanap',
+        demoUrl: '',
+        codeUrl: 'https://github.com/Arnaud293/P5_OC_KANAP'
+    },
+    {
+        id: 8,
+        imgUrl:'../assets/portfolio/ohmyfood.png',
+        title: 'Oh My Food',
+        demoUrl: 'https://arnaud293.github.io/OhMyFood_P3_OC/',
+        codeUrl: 'https://github.com/Arnaud293/OhMyFood_P3_OC'
+    },
+    {
+        id: 9,
+        imgUrl:'../assets/portfolio/panthere.png',
+        title: 'La Panthère',
+        demoUrl: 'https://arnaud293.github.io/Projet4_OC_LaPanthere/',
+        codeUrl: 'https://github.com/Arnaud293/Projet4_OC_LaPanthere'
+    }
 ]
