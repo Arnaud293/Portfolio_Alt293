bfa5e47e95d8c2cef10c0c8e632398942813bc13
Arnaud293 <arnaudlt293@gmail.com>
2022-12-27 16:49:16 +0100

    remove Link, add a tag, render demo icon + link only if it exists

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { FaGithub } from 'react-icons/fa';
 import { IoEyeSharp } from 'react-icons/io5';
 
-const ProjectCard = ({ project }) => 
-  (
+const ProjectCard = ({ project }) => {
+ return (
     <div className="shadow-md shadow-gray-700 rounded-lg">
     <img src={project.imgUrl} alt={project.title} className='rounded-md duration-200 hover:scale-105 w-full h-[65%]'/>
     <div className='flex items-center justify-center'>
-        <button className=' flex justify-center w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{<IoEyeSharp size={30} />}</button>
-        <button className=' flex justify-center w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{<FaGithub size={30} />}</button>
+        {project.demoUrl && (
+        <a href={project.demoUrl} target="_blank" className='flex justify-center w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
+        <button>{<IoEyeSharp size={30} />}</button>
+        </a>
+        )}
+        <a href={project.codeUrl} target="_blank" className='flex justify-center w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
+        <button>{<FaGithub size={30} />}</button>
+        </a>
     </div>
     </div>          
   )
 
-
+}
 export default ProjectCard
